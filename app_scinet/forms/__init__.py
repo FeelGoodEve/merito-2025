5d6594dcc30621d88181f86ab882647542102081
dead.color <dead.color@protonmail.com>
2025-04-27 19:29:44 +0200

    Dodaj komentarze do kodu
    # Conflicts:
    #	app_scinet/forms/__init__.py

--- app_scinet/forms/__init__.py
+++ app_scinet/forms/__init__.py
@@ -1,4 +1,5 @@
 from .CustomUserRegistrationForm import CustomUserRegistrationForm
 from .CommentForm import CommentForm
 
+# Definiuje co będzie zaimportowane przez "from app_scinet.forms import *"
 __all__ = ["CustomUserRegistrationForm", "CommentForm"]
