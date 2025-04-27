9da7be8a6cb337c4f8617f69bf68c295cf01df9b
dead.color <dead.color@protonmail.com>
2025-04-27 19:29:44 +0200

    Dodaj komentarze do kodu

--- app_scinet/forms/__init__.py
+++ app_scinet/forms/__init__.py
@@ -1,3 +1,4 @@
 from .CustomUserRegistrationForm import CustomUserRegistrationForm
 
+# Definiuje co będzie zaimportowane przez "from app_scinet.forms import *"
 __all__ = ["CustomUserRegistrationForm"]
